5ed8a34f452e377e31f47a5e2f33fe2f8d2a7c01
preethamkusuma <145964299+preethamkusuma@users.noreply.github.com>
2023-09-25 15:18:14 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# binary number
+# implicit
+a = 0b1111
+print(a)
+print(type(a))
+# explicit
+a = bin(0b1110)
+print(a)
+print(type(a))
+# data type annotation
+a =bin = 0b11100
+print(a)
+print(type(a))
+print("\n")
+# octal number system
+# implicit
+a = 0o1342
+print(a)
+print(type(a))
+# explicit
+a = oct(0o4322)
+print(a)
+print(type(a))
+# data type annotation
+a = oct = 0o11342
+print(a)
+print(type(a))
+print("\n")
+# hexa-dec
+# implicit
+b = 0xea12
+print(b)
+print(type(b))
+# explicit
+b = hex(0x57de)
+print(b)
+print(type(b))
+# data type annotation
+b = hex= 0xfa5822
+print(b)
+print(type(b))
